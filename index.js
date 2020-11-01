35b50f48729907ab65deb87e883b5f85133fcf06
ravikantSTAR <ravikant.rajput99@gmail.com>
2020-11-01 20:32:37 +0530

    second commit

--- index.js
+++ index.js
@@ -16,5 +16,5 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-export { Capabilities } from '../../../types/capabilities';
+//export { Capabilities } from '../../../types/capabilities';
 export { CapabilitiesService } from './capabilities_service';
